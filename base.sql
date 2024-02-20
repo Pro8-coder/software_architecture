09c3e14f7f054a0c5dfa4cf56e436fcbf26c4863
Pro8 <111078922+Pro8-coder@users.noreply.github.com>
2024-02-20 16:59:13 +0300

    Pro8

---
+++ base.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `Robot` DROP FOREIGN KEY `Robot_fk0`;
+
+ALTER TABLE `GroupUser` DROP FOREIGN KEY `GroupUser_fk0`;
+
+ALTER TABLE `GroupUser` DROP FOREIGN KEY `GroupUser_fk1`;
+
+ALTER TABLE `GroupUser` DROP FOREIGN KEY `GroupUser_fk2`;
+
+ALTER TABLE `Schedule` DROP FOREIGN KEY `Schedule_fk0`;
+
